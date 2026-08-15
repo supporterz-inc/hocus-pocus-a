14f30078cb53ec757c27fc5762e84e0d1ad9b501
takebo7979 <170819548+takebo7979@users.noreply.github.com>
2026-08-15 15:01:43 +0900

    ファイル保存の確認テスト

---
+++ src/models/knowledge.repository.test.ts
@@ -0,0 +1,22 @@
+import { readFile, unlink} from 'node:fs/promises';
+import { describe, expect, it} from 'vitest';
+import { Knowledge } from './knowledge.model.js';
+import { KnowledgeRepository } from './knowledge.repository.js';
+
+describe('KnowledgeRepository', () => {
+  it('Knowledge を保存できる', async() => {
+    const knowledge = Knowledge.create('#本文','test-auther');
+
+    const path =`./storage/${knowledge.knowledgeId}.json`; //保存先を決める
+    try{
+        await KnowledgeRepository.upsert(knowledge,);
+        const savedFile = JSON.parse(await readFile(path,'utf-8'));
+        
+        expect(savedFile).toEqual(knowledge);
+    }
+    finally{
+        await unlink(path).catch(() => {});
+    }
+});
+});
+
