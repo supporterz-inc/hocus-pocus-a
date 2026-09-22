176ab6bacb383bae6687e69475ad92d249e9c2cd
murahide104 <hidetoshi0623www@gmail.com>
2026-09-22 15:33:50 +0900

    feat: POST /new のエンドポイントを作成

--- src/controllers/create-knowledge.controller.tsx
+++ src/controllers/create-knowledge.controller.tsx
@@ -1,5 +1,12 @@
-// import { CreateKnowledgeFeature } from '../features/CreateKnowledgeFeature.js';
+import { CreateKnowledgeFeature } from '../features/CreateKnowledgeFeature.js';
+import { Knowledge } from '../models/knowledge.model.js';
+import { KnowledgeRepository } from '../models/knowledge.repository.js';
 
-// export function createKnowledgeController() {
-//   return <CreateKnowledgeFeature />;
-// }
+export function createKnowledgeController() {
+  return <CreateKnowledgeFeature />;
+}
+
+export async function saveKnowledgeController(content: string, authorId: string): Promise<void> {
+  const knowledge = Knowledge.create(content, authorId);
+  await KnowledgeRepository.upsert(knowledge);
+}
